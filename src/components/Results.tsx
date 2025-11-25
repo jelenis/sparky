b3a7b904bfcc76464a8a65f6380cacc7b50d2a19
John-Anthony <johnanthonyelenis@gmail.com>
2025-11-24 23:13:35 -0500

    minor ui adjustments

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -43,18 +43,24 @@ export default function Results({ inputs }: { inputs: ComputeProps }) {
   const displayContent = () => {
     if (calculationResult === "success") {
       return (
-        <div className="w-full flex justify-center">
-          <div className="stats shadow w-lg">
+        <div className="flex justify-center ">
+          <div className="stats mr-4">
             <div className="stat place-items-center">
               <div className="stat-title">Wire Size</div>
-              <div className="stat-value ">{wireSizeOutput}</div>
-              {/* <div className="stat-desc ">{wireSizeOutput.split(" ").slice(1)}</div> */}
+              <div className="stat-value">{wireSizeOutput}</div>
+              <div className="stat-desc">&nbsp;</div>
             </div>
             
             <div className="stat place-items-center">
               <div className="stat-title">Voltage Drop</div>
-              <div className="stat-value text-success">{`${voltageOutput.toFixed(2)} V`}</div>
-              <div className="stat-desc text-success" >{`${(voltageOutput / volts * 100).toFixed(2)} %`}</div>
+              <div className="stat-value">{`${voltageOutput.toFixed(2)} V`}</div>
+              <div className="stat-desc">{`${(voltageOutput / volts * 100).toFixed(2)} %`}</div>
+            </div>
+            
+            <div className="stat place-items-center">
+              <div className="stat-title">Voltage at Load</div>
+              <div className="stat-value text-success">{`${(volts - voltageOutput).toFixed(2)} V`}</div>
+              <div className="stat-desc">&nbsp;</div>
             </div>
           </div>
         </div>
@@ -75,9 +81,9 @@ export default function Results({ inputs }: { inputs: ComputeProps }) {
   };
 
   return (
-    <Card className="col-span-full results-card xl:max-w-none">
-      <h3 className="text-xl font-bold">Results</h3>
-      <div className="divider mb-4"></div>
+    <Card className="col-span-full results-card xl:max-w-none ">
+      <h3 className="text-2xl font-bold text-center">Results</h3>
+      <div className="divider mb-8"></div>
       {displayContent()}
     </Card>
   );
